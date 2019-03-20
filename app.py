23cbf6babc7199de0ba72c7fdde2e6a13ca0fe50
Victor Guerra (trc6014724) <victor.guerra@base2.com.br>
2019-03-20 12:53:01 -0300

    Commit inicial MySQL works!
    Signed-off-by: Victor Guerra (trc6014724) <victor.guerra@base2.com.br>

---
+++ app.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+import os
+from flask import Flask, request, jsonify
+from validate import json
+from db import executa_query
+
+app = Flask(__name__)
+
+tipos_servidores = ('MySQL')
+json = \
+{
+	"conexao" : 
+	{
+		"type" : "Tipo de servidor de banco de dados, um dos" + str(tipos_servidores),
+		"host" : "Nome ou IP do servidor de banco de dados",
+		"database" : "Nome do banco de dados",
+		"user" : "Usuário para conexão",
+		"password" : "Senha para conexão"
+	},
+	"query" : "Comando SQL para execução"
+}
+
+@app.route("/")
+def hello():
+    return "Olá mundo!!"
+
+@app.route("/banco-dados/run", methods=['GET', 'POST'])
+def run():
+    if request.method == 'GET':
+        return jsonify(json)
+    elif request.method == 'POST':
+        dados_conexao = request.get_json()['conexao']
+        query = request.get_json()['query']
+        retorno = executa_query(dados_conexao, query)
+        return jsonify(retorno)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ db.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+import sys
+import pymysql
+
+def obtem_conexao(dados_conexao):
+    if dados_conexao['type'] == 'MySQL':
+        return obtem_conexao_mysql(dados_conexao)
+
+def obtem_conexao_mysql(dados_conexao):
+    conexao = pymysql.connect(host=dados_conexao['host'],
+                             user=dados_conexao['user'],
+                             password=dados_conexao['password'],
+                             db=dados_conexao['database'],
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+    return conexao
+
+def executa_query(dados_conexao, query):
+    conexao = obtem_conexao(dados_conexao)
+    retorno = {}
+    linhas_afetadas = 0
+    try:
+        with conexao.cursor() as cursor:
+            linhas_afetadas = cursor.execute(query)
+            retorno["linhas_afetadas"] = linhas_afetadas
+            if 'SELECT' in str(query).upper():
+                dados = cursor.fetchall()
+                retorno["dados"] =  dados
+        conexao.commit()
+    except:
+        err = sys.exc_info()[1]
+        retorno["mensagem"] = "Erro ao executar a query [" + query + "]"
+        retorno["erro"] = str(err)
+        conexao.rollback()
+    finally:
+        conexao.close()
+    return retorno
